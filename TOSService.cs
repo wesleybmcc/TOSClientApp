1802933a6b02837d00557f20584575f29bd0c83d
Wesley McCammon <wesleybmcc@outlook.com>
2022-02-08 14:54:03 -0600

    Initial

---
+++ TOSService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThinkOrSwim;
+
+namespace TOSClientApp
+{
+    internal class TOSService
+    {
+        private static readonly TOSWorker tosWorker = new TOSWorker();
+
+        public TOSService()
+        {
+        }
+
+        public void Start()
+        {
+            new Task(tosWorker.Run).Start();
+        }
+
+        public void Stop()
+        {
+            tosWorker.Dispose();
+        }
+    }
+}
---
+++ ThinkOfSwim/TOSWorker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+using ThinkOrSwim;
+
+namespace ThinkOrSwim
+{
+    internal class TOSWorker : IDisposable
+    {
+        public bool KeepAlive { get; set; } = true;
+        private Client client = new Client();
+
+        public void Run()
+        {
+            new string[] { "EUR/USD", "USD/JPY", "GBP/USD", "USD/CHF", "AUD/USD", "USD/CAD" }.ToList().ForEach(instrument => {
+                client.Add(instrument, QuoteType.Bid);
+                client.Add(instrument, QuoteType.Ask);
+            }); 
+            
+            while (KeepAlive)
+            {
+                foreach (var quote in client.Quotes())
+                {
+                    string message = string.Format("{0} {1} {2}", quote.Symbol,
+                        quote.Type.ToString(), quote.Value);
+                    Console.WriteLine(message);
+
+                    var factory = new ConnectionFactory() { HostName = "localhost" };
+                    using (var connection = factory.CreateConnection())
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.QueueDeclare(queue: "hello",
+                                             durable: false,
+                                             exclusive: false,
+                                             autoDelete: false,
+                                             arguments: null);
+
+                        var body = Encoding.UTF8.GetBytes(message);
+
+                        channel.BasicPublish(exchange: "",
+                                             routingKey: "hello",
+                                             basicProperties: null,
+                                             body: body);
+                    }
+
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
